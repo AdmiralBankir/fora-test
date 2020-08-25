3c97428f0b14cb4cb057f56abaae1404d17722be
Владислав Бодин <vladislavbodin@MacBook-Pro-Vladislav.local>
2020-08-25 17:14:59 +0300

    Добавляет build

---
+++ build/precache-manifest.60ca95c7f286b6aff44d34f977ae5015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdad41a29cdd34246cd2c94d5cb38d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a680d5ddf9206fd561b4",
+    "url": "/static/css/main.2e2520f6.chunk.css"
+  },
+  {
+    "revision": "be0c023cbaf57a571d16",
+    "url": "/static/js/2.d7ae2d3f.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.d7ae2d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a680d5ddf9206fd561b4",
+    "url": "/static/js/main.8efaec61.chunk.js"
+  },
+  {
+    "revision": "750c15fc0e0707ffe08c",
+    "url": "/static/js/runtime-main.54251f8a.js"
+  }
+]);
